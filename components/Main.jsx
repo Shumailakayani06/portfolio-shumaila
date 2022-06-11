dc90c666832c9d472717b6b3763882f71da50c10
Shumaila Kayani <shumailakayani059@gmail.com>
2022-06-11 22:29:50 +0200

    fixed buggs

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,14 +8,14 @@ const Main = () => {
     <div className='w-full h-screen text-center'>
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
-              <p className='uppercase text-sm tracking-widest text-gray-600'>Let's Build Something Together</p>
+              <p className='uppercase text-sm tracking-widest text-gray-600'>Lets Build Something Together</p>
               <h1 className='py-4 text-gray-700'>
-                  Hi, I'm <span className='text-[#5651e5]'>Shumaila</span>
+                  Hi, I am <span className='text-[#5651e5]'>Shumaila</span>
               </h1>
               <h1 className='py-2 text-gray-700'>A Front-End Developer</h1>
               <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
-                  I'm a front-end web developer specializing in building exceptional digital experiences.
-                  Currently, i'm focused on building responsive front-end web applications while learning back-end technologies.
+                  I am a front-end web developer specializing in building exceptional digital experiences.
+                  Currently, I am focused on building responsive front-end web applications while learning back-end technologies.
               </p>
               <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                   <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
